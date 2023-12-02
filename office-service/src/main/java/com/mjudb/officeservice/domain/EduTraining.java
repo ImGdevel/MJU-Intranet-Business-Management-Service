6603073f1a14da0c6aa31024541d723a1b3db7d6
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2023-12-03 00:04:00 +0900

    Merge pull request #2 from ImGdevel/backend_sww
    Domain add

---
+++ office-service/src/main/java/com/mjudb/officeservice/domain/EduTraining.java
@@ -0,0 +1,39 @@
+package com.mjudb.officeservice.domain;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "교육훈련")
+@ToString
+public class EduTraining {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="교육훈련번호", columnDefinition = "NUMBER(5,0)")
+    @ApiModelProperty(example = "0123456789")
+    private Long eduT_num;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JoinColumn(name ="직원번호", columnDefinition = "NUMBER(5,0)")
+    @ApiModelProperty(example = "12345")
+    private Long emp_num;
+
+    @Column(name = "교육훈련유형", columnDefinition = "VARCHAR2(50)")
+    @ApiModelProperty(example = "온보딩 교육")
+    private String eduT_type;
+
+    @Column(name = "교육훈련일자", columnDefinition = "DATE")
+    @ApiModelProperty(example = "2023-10-11")
+    private String eduT_date;
+
+    @Column(name = "교육훈련내용", columnDefinition = "VARCHAR2(100)")
+    @ApiModelProperty(example = "뭔가 훈련같은 내용")
+    private String eduT_contents;
+}
---
+++ office-service/src/main/java/com/mjudb/officeservice/domain/Role.java
@@ -0,0 +1,25 @@
+package com.mjudb.officeservice.domain;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "직무")
+@ToString
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="직무번호", columnDefinition = "NUMBER(5,0)")
+    @ApiModelProperty(example = "01234")
+    private Long role_num;
+
+    @Column(name ="직무이름", columnDefinition = "VARCHAR2(50)")
+    @ApiModelProperty(example = "PM")
+    private String role_name;
+}
