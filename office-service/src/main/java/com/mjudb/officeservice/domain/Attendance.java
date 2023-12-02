0e8cf783341af042b98ae0f8fecbb34bc9fef085
Seo_ww <109172519+sdyt862@users.noreply.github.com>
2023-12-02 23:59:23 +0900

    Domain add
    근태, 교육훈련, 직무, 일정, 기술

---
+++ office-service/src/main/java/com/mjudb/officeservice/domain/Attendance.java
@@ -0,0 +1,35 @@
+package com.mjudb.officeservice.domain;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "근태")
+@ToString
+public class Attendance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="날짜", columnDefinition = "DATE")
+    @ApiModelProperty(example = "2023-11-12")
+    private String date;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JoinColumn(name ="직원번호", columnDefinition = "NUMBER(5,0)")
+    @ApiModelProperty(example = "12345")
+    private Long emp_num;
+
+    @Column(name = "출근시각", columnDefinition = "DATE")
+    @ApiModelProperty(example = "2023-10-11 00:01:12")
+    private String work_start_time;
+
+    @Column(name = "퇴근시각", columnDefinition = "DATE")
+    @ApiModelProperty(example = "2023-10-11 00:11:33")
+    private String work_end_time;
+}
---
+++ office-service/src/main/java/com/mjudb/officeservice/domain/Schedule.java
@@ -0,0 +1,39 @@
+package com.mjudb.officeservice.domain;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "일정")
+@ToString
+public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="일정번호", columnDefinition = "NUMBER(10,0)")
+    @ApiModelProperty(example = "0123456789")
+    private Long sche_num;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JoinColumn(name ="직원번호", columnDefinition = "NUMBER(5,0)")
+    @ApiModelProperty(example = "12345")
+    private Long emp_num;
+
+    @Column(name = "시작일자", columnDefinition = "DATE")
+    @ApiModelProperty(example = "2023-10-11")
+    private String sche_start_date;
+
+    @Column(name = "종료일자", columnDefinition = "DATE")
+    @ApiModelProperty(example = "2023-10-11")
+    private String sche_end_date;
+
+    @Column(name = "일정내용", columnDefinition = "VARCHAR2(50)")
+    @ApiModelProperty(example = "뭔가 일정같은 내용")
+    private String sche_contents;
+}
