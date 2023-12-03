4426a9899153234f30b05049506d1284029414d7
junewon <wns1628@gmail.com>
2023-12-03 15:00:46 +0900

    list type modification

--- office-service/src/main/java/com/mjudb/officeservice/repository/ProjectParticipantRepository.java
+++ office-service/src/main/java/com/mjudb/officeservice/repository/ProjectParticipantRepository.java
@@ -1,6 +1,7 @@
 package com.mjudb.officeservice.repository;
 
 import com.mjudb.officeservice.domain.Project;
+import com.mjudb.officeservice.domain.ProjectParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,5 +12,5 @@ import java.util.List;
 @Repository
 public interface ProjectParticipantRepository extends JpaRepository<Project, Long> {
     @Query("select pp from ProjectParticipant pp where pp.proj_num = :proj_num")
-    List<Project> findByProjNum(@Param("proj_num") Long proj_num);
+    List<ProjectParticipant> findByProjNum(@Param("proj_num") Long proj_num);
 }
--- office-service/src/main/java/com/mjudb/officeservice/repository/ScheduleRepository.java
+++ office-service/src/main/java/com/mjudb/officeservice/repository/ScheduleRepository.java
@@ -1,6 +1,7 @@
 package com.mjudb.officeservice.repository;
 
 import com.mjudb.officeservice.domain.Project;
+import com.mjudb.officeservice.domain.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,5 +16,5 @@ public interface ScheduleRepository extends JpaRepository<Project, Long> {
             "AND (\n" +
             "    (s.sche_start_date >= :proj_start AND s.sche_start_date <= :proj_end)\n" +
             "    OR (s.sche_end_date >= :proj_start AND s.sche_end_date <= :proj_end)\n)")
-    List<Project> findByDate(@Param("proj_num") Long proj_num, @Param("proj_start") String proj_start, @Param("proj_end") String proj_end);
+    List<Schedule> findByDate(@Param("proj_num") Long proj_num, @Param("proj_start") String proj_start, @Param("proj_end") String proj_end);
 }
