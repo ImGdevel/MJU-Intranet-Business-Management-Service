57b8ad537286f6d328f76eeefdf3051646205273
junewon <wns1628@gmail.com>
2023-12-03 15:34:02 +0900

    projectController

--- office-service/src/main/java/com/mjudb/officeservice/controller/ProjectController.java
+++ office-service/src/main/java/com/mjudb/officeservice/controller/ProjectController.java
@@ -1,15 +1,28 @@
 package com.mjudb.officeservice.controller;
 
+import com.mjudb.officeservice.domain.Project;
 import com.mjudb.officeservice.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/project")
 public class ProjectController {
     private final ProjectService projectService;
 
     @Autowired
     public ProjectController(ProjectService projectService){
         this.projectService = projectService;
+    }
+
+    @GetMapping("/{date}")
+    public List<Project> findByDate(@PathVariable(name = "date") String date){
+        return projectService.findByDate(date);
     }
 }
