e9ad7243672b38f6741c770c6902ed6a71eba8e6
Seo_ww <109172519+sdyt862@users.noreply.github.com>
2023-12-03 22:18:41 +0900

    cors 허용설정
    @CrossOrigin 추가

--- office-service/src/main/java/com/mjudb/officeservice/controller/CustomerController.java
+++ office-service/src/main/java/com/mjudb/officeservice/controller/CustomerController.java
@@ -4,11 +4,9 @@ import com.mjudb.officeservice.service.CustomerService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/customer")
 @Api(tags = {"발주처 정보 API"})
